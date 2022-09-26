40337c444209a2fefdac03e88c5fe1bf783945ae
MinhK66Uet <21020023@vnu.edu.vn>
2022-09-26 08:32:33 +0700

    Up 26/9 2
    Thêm điều khiển bằng mũi tên.

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -6,4 +6,6 @@ public class Constants {
     public static final int t_red = 255, t_green = 0, t_blue =255;
     public static final int bombsCode = 88;
 
+    public static final int timeBomb = 3500;
+
 }
